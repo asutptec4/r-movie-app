3c4346d2a6ddb818e48c3fcc34b0a4be58256e2f
Aliaksandr Shyshonak <asutp_tec4@tut.by>
2021-02-14 18:07:30 +0300

    build: add babel and webpack config

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      { test: /\.(js)$/i, use: "babel-loader" },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".json", ".js", ".jsx"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "NetflixRoulette",
+    }),
+  ],
+};
