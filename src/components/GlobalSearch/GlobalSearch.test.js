b0a8448fc6f650a09dfa5a4fa4e3276274fe6a7b
Aliaksandr Shyshonak <asutp_tec4@tut.by>
2021-04-19 19:55:47 +0300

    test: make coverage more than 90

--- src/components/GlobalSearch/GlobalSearch.test.js
+++ src/components/GlobalSearch/GlobalSearch.test.js
@@ -1,13 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import 'regenerator-runtime/runtime';
 
-import store from '../../store/store';
 import GlobalSearch from './GlobalSearch';
 
+const mockStore = configureMockStore([thunk]);
+
 describe('<GlobalSearch />', () => {
-  test('should render search form', () => {
+  test('should render search form and make search by route', async () => {
+    const store = mockStore({
+      movies: {
+        searchText: '',
+      },
+    });
     render(
       <Provider store={store}>
         <MemoryRouter initialEntries={['/search/transformers']}>
@@ -16,5 +26,36 @@ describe('<GlobalSearch />', () => {
       </Provider>,
     );
     expect(screen.getByText('Find your movie')).toBeInTheDocument();
+    await waitFor(() =>
+      expect(store.getActions()).toContainEqual(
+        expect.objectContaining({
+          type: 'movies/setSearchText',
+        }),
+      ),
+    );
+  });
+
+  test('should render search form by enter click', async () => {
+    const store = mockStore({
+      movies: {
+        searchText: '',
+      },
+    });
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/movies']}>
+          <GlobalSearch />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(screen.getByText('Find your movie')).toBeInTheDocument();
+    userEvent.type(screen.getByPlaceholderText(/search/i), 'test{enter}');
+    await waitFor(() =>
+      expect(store.getActions()).toContainEqual(
+        expect.objectContaining({
+          type: 'movies/setSearchText',
+        }),
+      ),
+    );
   });
 });
